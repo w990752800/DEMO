668748ad613a233293995aa60f580826920acb47
w990752800 <w191604989>
2017-08-09 17:15:36 +0800

    添加todomvc文件夹

---
+++ angular_dome/todomvc/js/app.js
@@ -0,0 +1,77 @@
+(function (angular) {
+	'use strict';
+	//主程序
+	var app = angular.module('myApp', []);
+	//注册一个控制器
+	app.controller('myController', ['$scope', function ($scope) {
+		//给文本框添加一个模型
+		$scope.txt = '';
+		//任务列表也需要一个
+		//每个任务的结构{id:1,txt:内容,completed:true}
+		$scope.todos = [
+			{id: 0.2356, txt: '吃饭', completed: false},
+			{id: 0.14785, txt: '睡觉', completed: false},
+			{id: 0.3698, txt: '打豆豆', completed: true}
+		];
+		//添加add
+		$scope.add = function () {
+			if(!$scope.txt){
+				return;
+			}
+			$scope.todos.push({
+				id: Math.random(),
+				txt: $scope.txt,
+				completed: false
+			});
+			$scope.txt = '';
+		};
+		//删除
+		$scope.remove = function (id) {
+			for (var i = 0; i < $scope.todos.length; i++) {
+				if($scope.todos[i].id === id){
+					$scope.todos.splice(i,1);
+					break;
+				}
+			}
+		};
+		//删除全部
+		$scope.clear = function(){
+			var result = [];
+			for (var i = 0; i < $scope.todos.length; i++) {
+				if(!$scope.todos[i].completed){
+					result.push($scope.todos[i]);
+				}
+			}
+			$scope.todos = result;
+		};
+		//是否有已完成的
+		$scope.existCompleted=function(){
+			for (var i = 0; i < $scope.todos.length; i++) {
+				if($scope.todos[i].completed){
+					return true;
+				}
+			}
+			return false;
+		};
+		//判断当前选中的
+		$scope.current = -1;
+		$scope.currentDb = function(id){
+			$scope.current = id;
+		};
+		$scope.save = function(id){
+			$scope.current = -1;
+		};
+
+		var now = true;
+		$scope.toggleAll = function(){
+			for(var i= 0;i<$scope.todos.length;i++){
+				$scope.todos[i].completed = now;
+			}
+			now = !now;
+		}
+
+
+
+	}]);
+
+})(angular);
