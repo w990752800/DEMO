6a9db9dcf02134eee397f533cea0372386029513
w990752800 <w191604989>
2017-08-22 15:13:50 +0800

    8-22更新

---
+++ bootstrap/果酱音乐/js/main.js
@@ -0,0 +1,9 @@
+/**
+ * Created by Personal on 2017/8/18.
+ */
+$(function(){
+    $("img.lazyPic").lazyload({   //可以在img位置指定要实现本效果的页面区域，
+        effect : "fadeIn", //实现图像淡入效果
+        failurelimit : 2000 //因页面的框架结构可能不规则，此处建议添加强制加载的图片数，保证图片能正常加载。
+    });
+});
